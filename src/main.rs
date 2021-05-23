3980eb6743ddce3d775846b062066f011ef21d7b
Rafael Rondao <rafael.rondao@gmail.com>
2021-05-23 10:20:49 -0300

    Clean egui template for zen_sm
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let app = egui_template::TemplateApp::default();
-    eframe::run_native(Box::new(app));
+    let app = zen_sm::ZenSM::default();
+    eframe::run_native(Box::new(app), eframe::NativeOptions::default());
 }
