00770f5f5f78c9a85805b0db1e88a3fd081d93ee
Rafael Rondao <rafael.rondao@gmail.com>
2021-05-24 21:11:08 -0300

    Keep selected color with white rectangle
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/widgets/palette.rs
+++ src/widgets/palette.rs
@@ -6,18 +6,18 @@ use zen::graphics;
 
 pub struct Palette {
     texture_id: Option<egui::TextureId>,
-    color: [f32; 3],
     color_popup_id: egui::Id,
-    selected_color: Option<egui::Pos2>,
+    selected_color: [f32; 3],
+    selected_position: Option<egui::Pos2>,
 }
 
 impl Default for Palette {
     fn default() -> Self {
         Self {
             texture_id: None,
-            color: [0.0; 3],
+            selected_color: [0.0; 3],
             color_popup_id: egui::Id::new("palette_color_popup_id"),
-            selected_color: None,
+            selected_position: None,
         }
     }
 }
@@ -67,55 +67,73 @@ impl Palette {
             );
             let from_screen = to_screen.inverse();
 
-            // Draw a rectangle on hovered color.
-            if let Some(hover_pos) = response.hover_pos() {
-                let palette_pos = (from_screen * hover_pos).floor();
-                let selection_pos = to_screen * palette_pos;
+            let mut selected_position = None;
+            if self.selected_position.is_none() {
+                // Select the hovering position.
+                if let Some(hover_pos) = response.hover_pos() {
+                    let color_pos = (from_screen * hover_pos).floor();
+                    // Handle only positions inside the Widget. Use epsilon to avoid on bounds.
+                    selected_position = if widget_rect
+                        .contains(to_screen * color_pos + egui::Vec2 { x: 0.1, y: 0.1 })
+                    {
+                        Some(color_pos)
+                    } else {
+                        None
+                    }
+                };
+            } else {
+                selected_position = self.selected_position;
+            }
 
-                // Handle positions inside the Widget. Use epsilon to avoid on bound.
-                if widget_rect.contains(selection_pos + egui::Vec2 { x: 0.1, y: 0.1 }) {
-                    let rect = egui::Rect {
-                        min: selection_pos,
-                        max: selection_pos
-                            + egui::Vec2 {
-                                x: (size.x / 16.0),
-                                y: (size.y / 16.0),
-                            },
-                    };
+            if let Some(selected_position) = selected_position {
+                // Draw a rectangle around the selected color.
+                let rect = egui::Rect {
+                    min: to_screen * selected_position,
+                    max: to_screen * selected_position
+                        + egui::Vec2 {
+                            x: (size.x / 16.0),
+                            y: (size.y / 16.0),
+                        },
+                };
 
-                    let painter = ui.painter_at(widget_rect);
-                    painter.rect_stroke(rect, 1.0, egui::Stroke::new(2.0, egui::Color32::WHITE));
+                let painter = ui.painter_at(widget_rect);
+                painter.rect_stroke(rect, 1.0, egui::Stroke::new(2.0, egui::Color32::WHITE));
 
-                    if response.secondary_clicked() {
-                        let palette_color: graphics::Rgb888 = palette.sub_palettes
-                            [palette_pos.y as usize]
-                            .colors[palette_pos.x as usize]
-                            .into();
-                        self.color = [
-                            palette_color.r as f32 / 255.0,
-                            palette_color.g as f32 / 255.0,
-                            palette_color.b as f32 / 255.0,
-                        ];
-                        println!("{:?}", self.color);
-                        self.selected_color = Some(palette_pos);
+                // Open a color picker.
+                if response.secondary_clicked() {
+                    self.selected_position = Some(selected_position);
 
-                        if !ui.memory().is_popup_open(self.color_popup_id) {
-                            ui.memory().toggle_popup(self.color_popup_id);
-                        }
+                    let palette_color: graphics::Rgb888 = palette.sub_palettes
+                        [selected_position.y as usize]
+                        .colors[selected_position.x as usize]
+                        .into();
+
+                    self.selected_color = [
+                        palette_color.r as f32 / 255.0,
+                        palette_color.g as f32 / 255.0,
+                        palette_color.b as f32 / 255.0,
+                    ];
+                    println!("{:?}", self.selected_color);
+
+                    if !ui.memory().is_popup_open(self.color_popup_id) {
+                        ui.memory().toggle_popup(self.color_popup_id);
                     }
                 }
             }
 
-            self.ui_color_picker(ui, &mut response);
-            if response.changed() {
-                if let Some(selected_color) = self.selected_color {
-                    palette.sub_palettes[selected_color.y as usize].colors
-                        [selected_color.x as usize] = graphics::Rgb888 {
-                        r: (self.color[0] * 255.0) as u8,
-                        g: (self.color[1] * 255.0) as u8,
-                        b: (self.color[2] * 255.0) as u8,
+            // Color picker is open.
+            if ui.memory().is_popup_open(self.color_popup_id) {
+                self.ui_color_picker(ui, &mut response);
+                if response.changed() {
+                    if let Some(selected_position) = self.selected_position {
+                        palette.sub_palettes[selected_position.y as usize].colors
+                            [selected_position.x as usize] = graphics::Rgb888 {
+                            r: (self.selected_color[0] * 255.0) as u8,
+                            g: (self.selected_color[1] * 255.0) as u8,
+                            b: (self.selected_color[2] * 255.0) as u8,
+                        }
+                        .into();
                     }
-                    .into();
                 }
             }
         }
@@ -124,30 +142,29 @@ impl Palette {
     }
 
     fn ui_color_picker(&mut self, ui: &mut egui::Ui, response: &mut egui::Response) {
-        if ui.memory().is_popup_open(self.color_popup_id) {
-            let area_response = egui::Area::new(self.color_popup_id)
-                .order(egui::Order::Foreground)
-                .default_pos(response.rect.max)
-                .show(ui.ctx(), |ui| {
-                    ui.spacing_mut().slider_width = 256.0;
-                    containers::Frame::popup(ui.style()).show(ui, |ui| {
-                        let mut hsva = color::Hsva::from_rgb(self.color);
-                        if color_picker::color_picker_hsva_2d(
-                            ui,
-                            &mut hsva,
-                            color_picker::Alpha::Opaque,
-                        ) {
-                            self.color = hsva.to_rgb();
-                            response.mark_changed();
-                        }
-                    });
+        let area_response = egui::Area::new(self.color_popup_id)
+            .order(egui::Order::Foreground)
+            .default_pos(response.rect.max)
+            .show(ui.ctx(), |ui| {
+                ui.spacing_mut().slider_width = 256.0;
+                containers::Frame::popup(ui.style()).show(ui, |ui| {
+                    let mut hsva = color::Hsva::from_rgb(self.selected_color);
+                    if color_picker::color_picker_hsva_2d(
+                        ui,
+                        &mut hsva,
+                        color_picker::Alpha::Opaque,
+                    ) {
+                        self.selected_color = hsva.to_rgb();
+                        response.mark_changed();
+                    }
                 });
+            });
 
-            if !response.secondary_clicked()
-                && (ui.input().key_pressed(egui::Key::Escape) || area_response.clicked_elsewhere())
-            {
-                ui.memory().close_popup();
-            }
+        if !response.secondary_clicked()
+            && (ui.input().key_pressed(egui::Key::Escape) || area_response.clicked_elsewhere())
+        {
+            ui.memory().close_popup();
+            self.selected_position = None;
         }
     }
 }
