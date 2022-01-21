6881b10e036628646f2770cfa09d6aa31586bacf
Rafael Rondao <rafael.rondao@gmail.com>
2022-01-21 11:48:10 -0300

    Add menu button to save edited ROM to file
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/app.rs
+++ src/app.rs
@@ -38,20 +38,26 @@ impl epi::App for ZenSM {
         egui::TopBottomPanel::top("top_menu").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
-                    if ui.small_button("Load ROM file").clicked() {
+                    if ui.button("Load ROM from file").clicked() {
                         super::app::execute_async(async move {
                             if let Some(file) = rfd::AsyncFileDialog::new().pick_file().await {
                                 let file_data = file.read().await;
                                 *SELECTED_FILE_DATA.lock().unwrap() = Some(file_data);
                             }
                         });
+                        ui.close_menu();
+                    };
+                    if ui.button("Save ROM to file").clicked() {
+                        self.sm.save_to_rom();
+                        super::app::save_file(&self.sm.rom);
+                        ui.close_menu();
                     };
                 });
             });
         });
 
         if !self.palette.is_texture_loaded() {
-            if let Some(palette) = self.sm.palettes.get(&0xC2AE5D) {
+            if let Some(palette) = self.sm.palettes.get(&0xC2AD7C) {
                 self.palette.load_texture(frame, palette);
             }
         }
@@ -64,12 +70,12 @@ impl epi::App for ZenSM {
                     if self.palette.is_texture_loaded() {
                         let response = self.palette.ui(
                             ui,
-                            self.sm.palettes.get_mut(&0xC2AE5D).unwrap(),
+                            self.sm.palettes.get_mut(&0xC2AD7C).unwrap(),
                             Vec2 { x: 300.0, y: 150.0 },
                         );
                         if response.changed() {
                             self.palette
-                                .load_texture(frame, &self.sm.palettes[&0xC2AE5D]);
+                                .load_texture(frame, &self.sm.palettes[&0xC2AD7C]);
                         }
                     }
                 });
@@ -85,4 +91,42 @@ fn execute_async<F: Future<Output = ()> + Send + 'static>(f: F) {
 #[cfg(target_arch = "wasm32")]
 fn execute_async<F: Future<Output = ()> + 'static>(f: F) {
     wasm_bindgen_futures::spawn_local(f);
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn save_file(data: &Vec<u8>) {
+    if let Some(file) = rfd::FileDialog::new().save_file() {
+        if let Err(e) = std::fs::write(file, data) {
+            println!("{:?}", e);
+        }
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+fn save_file(data: &Vec<u8>) {
+    use wasm_bindgen::JsCast;
+    use web_sys::BlobPropertyBag;
+
+    // Create a JsArray for the ROM data.
+    let uint8arr = js_sys::Uint8Array::new(&unsafe { js_sys::Uint8Array::view(&data) }.into());
+    let js_bytes = js_sys::Array::new();
+    js_bytes.push(&uint8arr.buffer());
+
+    if let Ok(blob) = web_sys::Blob::new_with_u8_array_sequence_and_options(
+        &js_bytes,
+        BlobPropertyBag::new().type_("application/octet-stream"),
+    ) {
+        if let Ok(url) = web_sys::Url::create_object_url_with_blob(&blob) {
+            if let Some(window) = web_sys::window() {
+                if let Some(document) = window.document() {
+                    if let Some(element) = document.get_element_by_id("download_rom") {
+                        let anchor = element.dyn_into::<web_sys::HtmlAnchorElement>().unwrap();
+                        anchor.set_href(&url);
+                        anchor.click();
+                    }
+                }
+            }
+            web_sys::Url::revoke_object_url(&url).unwrap();
+        }
+    }
 }
