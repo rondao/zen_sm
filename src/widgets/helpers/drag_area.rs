02fbe0744b8a8f31fd25fe3912b169ea0d5e0310
Rafael Rondao <rafael.rondao@gmail.com>
2022-08-24 15:29:02 -0300

    Create a dragable area by secondary mouse button
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

---
+++ src/widgets/helpers/drag_area.rs
@@ -0,0 +1,38 @@
+use eframe::{
+    egui::{Event, PointerButton, Response, Sense, Ui},
+    epaint::{Rect, Vec2},
+};
+
+use super::zoom_area::ZoomArea;
+
+#[derive(Default)]
+pub struct DragArea {
+    zoom_area: ZoomArea,
+    dragging: bool,
+}
+
+impl DragArea {
+    pub fn create(&mut self, ui: &mut Ui, widget_size: Vec2) -> (Rect, Response) {
+        for event in &ui.input().events {
+            match event {
+                Event::PointerButton {
+                    pos: _,
+                    button: PointerButton::Secondary,
+                    pressed,
+                    modifiers: _,
+                } => self.dragging = *pressed,
+                _ => {}
+            }
+        }
+
+        self.zoom_area.create(
+            ui,
+            widget_size,
+            if self.dragging {
+                Sense::click_and_drag()
+            } else {
+                Sense::click()
+            },
+        )
+    }
+}
--- src/widgets/helpers/mod.rs
+++ src/widgets/helpers/mod.rs
@@ -1,3 +1,4 @@
+pub mod drag_area;
 pub mod selection;
 pub mod texture;
 pub mod zoom_area;
