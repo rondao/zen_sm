9d00fe4470a90ad187550925a561dcab144d37e9
Rafael Rondao <rafael.rondao@gmail.com>
2023-05-17 14:06:17 -0300

    Fix paning, now require pressing Space
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- src/widgets/helpers/drag_area.rs
+++ src/widgets/helpers/drag_area.rs
@@ -1,5 +1,5 @@
 use eframe::{
-    egui::{Event, PointerButton, Response, Sense, Ui},
+    egui::{CursorIcon, Key, Response, Sense, Ui},
     epaint::{Rect, Vec2},
 };
 
@@ -8,31 +8,26 @@ use super::zoom_area::ZoomArea;
 #[derive(Default)]
 pub struct DragArea {
     zoom_area: ZoomArea,
-    dragging: bool,
 }
 
 impl DragArea {
     pub fn create(&mut self, ui: &mut Ui, widget_size: Vec2) -> (Rect, Response) {
-        ui.input(|i| for event in &i.events {
-            match event {
-                Event::PointerButton {
-                    pos: _,
-                    button: PointerButton::Secondary,
-                    pressed,
-                    modifiers: _,
-                } => self.dragging = *pressed,
-                _ => {}
-            }
-        });
+        let paning = ui.input(|i| i.key_down(Key::Space));
 
-        self.zoom_area.create(
-            ui,
-            widget_size,
-            if self.dragging {
-                Sense::click_and_drag()
-            } else {
-                Sense::click()
-            },
-        )
+        let senses = if paning {
+            Sense::focusable_noninteractive()
+        } else {
+            Sense::click_and_drag()
+        };
+
+        let (rect, response) = self.zoom_area.create(ui, widget_size, senses);
+
+        let response = if paning {
+            response.on_hover_cursor(CursorIcon::Grab)
+        } else {
+            response
+        };
+
+        (rect, response)
     }
 }
