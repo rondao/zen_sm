adeb24bdf16bcb176997505aa40f979630869e52
Rafael Rondão <rafael.rondao@gmail.com>
2023-09-01 11:23:28 -0300

    Load all BTS icons for Wasm
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- src/assets.rs
+++ src/assets.rs
@@ -19,57 +19,67 @@ extern "C" {
 }
 
 #[cfg(target_arch = "wasm32")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+extern "C" {
+    async fn get_assets_names() -> wasm_bindgen::JsValue;
+}
+
+#[cfg(target_arch = "wasm32")]
 pub fn load_bts_icons(editor_assets: Arc<Mutex<HashMap<BtsTile, ColorImage>>>) {
     wasm_bindgen_futures::spawn_local(async move {
-        let path = "/images/1_13.png";
+        let js_assets_names: js_sys::JsString = get_assets_names().await.into();
+        let assets_names = js_assets_names.as_string().unwrap();
 
-        let js_buffer: js_sys::Uint8Array = fetch_image(path).await.into();
+        for asset in assets_names.lines() {
+            let js_buffer: js_sys::Uint8Array =
+                fetch_image(&format!("images/{}", asset)).await.into();
 
-        let mut buffer = vec![0; js_buffer.length() as usize];
-        js_buffer.copy_to(&mut buffer);
+            let mut buffer = vec![0; js_buffer.length() as usize];
+            js_buffer.copy_to(&mut buffer);
 
-        let result = image::load_from_memory(&buffer);
-        if let Ok(image) = result {
-            let size = [image.width() as _, image.height() as _];
-            let image_buffer = image.to_rgba8();
-            let pixels = image_buffer.as_flat_samples();
+            let result = image::load_from_memory(&buffer);
+            if let Ok(image) = result {
+                let size = [image.width() as _, image.height() as _];
+                let image_buffer = image.to_rgba8();
+                let pixels = image_buffer.as_flat_samples();
 
-            let file_name = path.split("/").last().unwrap().replace(".png", "");
-            let mut splitted_file_name = file_name.split("_");
+                let file_name = asset.replace(".png", "");
+                let mut splitted_file_name = file_name.split("_");
 
-            let bts_tile = BtsTile {
-                block_type: u8::from_str_radix(splitted_file_name.next().unwrap(), 16)
-                    .unwrap()
-                    .into(),
-                bts_block: u8::from_str_radix(splitted_file_name.next().unwrap(), 16).unwrap(),
-            };
+                let bts_tile = BtsTile {
+                    block_type: u8::from_str_radix(splitted_file_name.next().unwrap(), 16)
+                        .unwrap()
+                        .into(),
+                    bts_block: u8::from_str_radix(splitted_file_name.next().unwrap(), 16).unwrap(),
+                };
 
-            let bts_icon = ColorImage::from_rgba_unmultiplied(size, pixels.as_slice());
+                let bts_icon = ColorImage::from_rgba_unmultiplied(size, pixels.as_slice());
 
-            if bts_tile.block_type == BlockType::Slope {
-                editor_assets.lock().unwrap().insert(
-                    BtsTile {
-                        block_type: bts_tile.block_type,
-                        bts_block: bts_tile.bts_block | 0b01_0_00000,
-                    },
-                    image_mirror_horizontally(&bts_icon),
-                );
-                editor_assets.lock().unwrap().insert(
-                    BtsTile {
-                        block_type: bts_tile.block_type,
-                        bts_block: bts_tile.bts_block | 0b10_0_00000,
-                    },
-                    image_mirror_vertically(&bts_icon),
-                );
-                editor_assets.lock().unwrap().insert(
-                    BtsTile {
-                        block_type: bts_tile.block_type,
-                        bts_block: bts_tile.bts_block | 0b11_0_00000,
-                    },
-                    image_mirror_horizontally(&image_mirror_vertically(&bts_icon)),
-                );
+                if bts_tile.block_type == BlockType::Slope {
+                    editor_assets.lock().unwrap().insert(
+                        BtsTile {
+                            block_type: bts_tile.block_type,
+                            bts_block: bts_tile.bts_block | 0b01_0_00000,
+                        },
+                        image_mirror_horizontally(&bts_icon),
+                    );
+                    editor_assets.lock().unwrap().insert(
+                        BtsTile {
+                            block_type: bts_tile.block_type,
+                            bts_block: bts_tile.bts_block | 0b10_0_00000,
+                        },
+                        image_mirror_vertically(&bts_icon),
+                    );
+                    editor_assets.lock().unwrap().insert(
+                        BtsTile {
+                            block_type: bts_tile.block_type,
+                            bts_block: bts_tile.bts_block | 0b11_0_00000,
+                        },
+                        image_mirror_horizontally(&image_mirror_vertically(&bts_icon)),
+                    );
+                }
+                editor_assets.lock().unwrap().insert(bts_tile, bts_icon);
             }
-            editor_assets.lock().unwrap().insert(bts_tile, bts_icon);
         }
     });
 }
